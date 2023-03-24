1887479663c4f8db2e411ce4a60438241571af8e
wykdg <wykchina@gmail.com>
2023-03-24 14:29:47 +0800

    fix: rename

--- openwrt_route_add.sh
+++ openwrt_route_add.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
 #本地存储路径
-IP_FILE="/usr/share/scut_uniom/scut_route.txt"
+IP_FILE="/usr/share/scut_unicom/scut_route.txt"
 GATEWAY=`uci get network.wan.gateway`
 #以下是教育网内网地址分流
 cat $IP_FILE|while read -r IP MASK; do

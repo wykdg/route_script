bde48b51e2d85f0e829ba2ef4d60c46a7c079c71
wykdg <wykchina@gmail.com>
2023-04-10 17:26:08 +0800

    update script

--- openwrt_route_add.sh
+++ openwrt_route_add.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
+
+
+
+
 #本地存储路径
+if=`uci get network.unicom`
+if [ ! -n "$if" ]; then 
+    echo "create l2tp" 
+    uci set network.unicom=interface
+    uci set network.unicom.proto='l2tp'
+    uci set network.unicom.ipv6='auto'
+    uci set firewall.@zone[1].network='wan wan6 unicom'
+fi 
+    
+uci set network.unicom.server=$1
+uci set network.unicom.username=$2
+uci set network.unicom.password=$3
+uci commit
+
+ifup unicom
+
 IP_FILE="/usr/share/scut_unicom/scut_route.txt"
 GATEWAY=`uci get network.wan.gateway`
 #以下是教育网内网地址分流
